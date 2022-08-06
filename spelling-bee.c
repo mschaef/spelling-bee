f346277ec4ea4b9cf4833e0e2ca1414f332b51d6
Mike Schaeffer <mike@mschaef.com>
2022-08-06 16:52:56 -0400

    Break the list load and scan into two separate steps.

--- spelling-bee.c
+++ spelling-bee.c
@@ -6,7 +6,14 @@
 
 #define BUF_SIZE 64
 
+#define MAX_WORDS 262144
+
 typedef uint64_t letter_mask;
+
+struct word_info {
+     char word_letters[BUF_SIZE];
+     letter_mask mask;
+};
 
 letter_mask letter_to_mask(char letter) {
      assert(isalpha(letter));
@@ -30,10 +37,12 @@ letter_mask letters_to_mask(char *letters) {
      return m;
 }
 
-void scan_wordlist(letter_mask central_letter_mask, letter_mask board_mask) {
+size_t load_wordlist(struct word_info words[], size_t max_words) {
      FILE *f = fopen("wordlist-large", "r");
 
      char buf[BUF_SIZE];
+
+     size_t index = 0;
 
      while(fgets(buf, BUF_SIZE, f)) {
           size_t len =  strlen(buf) - 1;
@@ -43,19 +52,32 @@ void scan_wordlist(letter_mask central_letter_mask, letter_mask board_mask) {
                continue;
           }
 
-          letter_mask word_letters = letters_to_mask(buf);
+          strncpy(words[index].word_letters, buf, BUF_SIZE);
+          words[index].mask = letters_to_mask(buf);
+
+          index++;
+     }
+
+     fclose(f);
+
+     return index;
+}
+
+void scan_wordlist(struct word_info words[], size_t num_words, letter_mask central_letter_mask, letter_mask board_mask) {
+     for(size_t index = 0; index < num_words; index++) {
+          letter_mask word_letters = words[index].mask;
 
           if (word_letters & ~board_mask) {
                continue;
           }
 
           if (word_letters & central_letter_mask) {
-               printf("%s\n", buf);
+               printf("%s\n", words[index].word_letters);
           }
      }
-
-     fclose(f);
 }
+
+struct word_info words[MAX_WORDS];
 
 int main(int argc, char *argv[]) {
      if (argc < 2) {
@@ -63,9 +85,12 @@ int main(int argc, char *argv[]) {
           return 1;
      }
 
+
+     size_t num_words = load_wordlist(words, MAX_WORDS);
+
      char *letters = argv[1];
 
-     scan_wordlist(letter_to_mask(letters[0]), letters_to_mask(letters));
+     scan_wordlist(words, num_words, letter_to_mask(letters[0]), letters_to_mask(letters));
 
      return 0;
 }
